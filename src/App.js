0f850cd51b57f0a26d40147b7ee0c3a84fcf089b
Jordan Andrade <jordanandrade@andradedata.com>
2021-12-17 01:04:59 -0800

    commit on main

--- src/App.js
+++ src/App.js
@@ -1,11 +1,90 @@
-import * as React from "react";
-import { ethers } from "ethers";
+import React, { useEffect, useState } from "react";
 import './App.css';
+import twitterLogo from './assets/twitter-logo.svg';
 
-export default function App() {
+  // Constants
+  const TWITTER_HANDLE = 'Dape25';
+  const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
-  const wave = () => {
+  const App = () => {
+    // States
+    const [currentAccount, setCurrentAccount] = useState("");
+
+    // Actions
+    const checkIfWalletIsConnected = async () => {    
+      try {
+        const { ethereum } = window;
+  
+        if (!ethereum) {
+          console.log("Make sure you have metamask!");
+          return;
+        } else {
+          console.log("We have the ethereum object", ethereum);
+        }
+        
+        const accounts = await ethereum.request({ method: 'eth_accounts' });
+        
+        if (accounts.length !== 0) {
+          const account = accounts[0];
+          console.log("Found an authorized account:", account);
+          setCurrentAccount(account)
+        } else {
+          console.log("No authorized account found")
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
     
+    const connectWallet = async () => {
+      try {
+        const { ethereum } = window;
+  
+        if (!ethereum) {
+          alert("Get MetaMask!");
+          return;
+        }
+  
+        const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+  
+        console.log("Connected", accounts[0]);
+        setCurrentAccount(accounts[0]); 
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    const renderNotConnectedContainer = () => (
+      <button
+        className="walletButton"
+        onClick={connectWallet}
+      >
+        Connect MetaMask Wallet
+      </button>
+    );
+
+    const renderConnectedContainer = () => (
+      <div className="summary">
+          Add a link to your LinkedIn Profile to be displayed as a Web 3.0 dev!
+          
+        <button
+          className="profileButton"
+          onClick={postProfile}
+        >
+          Post LinkedIn
+        </button>
+
+      </div>
+    );
+
+    // Use Effects
+    useEffect(() => {
+      console.log("Web 3.0 LinkedIn")
+      checkIfWalletIsConnected();
+    }, [])
+  
+  const postProfile = () => {
+    console.log("Nice try")
   }
   
   return (
@@ -15,19 +94,28 @@ export default function App() {
         <div className="dataContainer">
 
           <div className="header">
-          Web 3.0 LinkedIn Profiles
-          </div>
+          Web 3.0 LinkedIn
+          </div>       
 
-          <div className="summary">
-          Add a link to your LinkedIn Profile to be displayed as a Web 3.0 dev!
-          </div>
+          {!currentAccount && renderNotConnectedContainer()}
 
-          <button className="profileButton" onClick={wave}>
-            Post LinkedIn
-          </button>
-          
+          {currentAccount && renderConnectedContainer()}
+
         </div>
+
+        <div className="footer-container">
+          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+          <a
+            className="footer-text"
+            href={TWITTER_LINK}
+            target="_blank"
+            rel="noreferrer"
+          >{`@${TWITTER_HANDLE}`}</a>
+        </div>
+        
       </div>
     </div>
   );
 }
+
+export default App
